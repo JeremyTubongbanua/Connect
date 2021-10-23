473ae93a9d59c112f6057bbaad7da18ae9e4b2e2
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2021-10-23 16:03:49 -0400

    Worked on screns

---
+++ app/components/Event.js
@@ -0,0 +1,10 @@
+
+
+import React from 'react';
+import { View, Image, Text, Pressable, Button, StyleSheet, TextInput } from 'react-native';
+
+const Event = ({ title, startTime, endTime }) => {
+
+}
+
+export default Event;
--- app/screens/Socials.js
+++ app/screens/Socials.js
@@ -1,12 +1,45 @@
 
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, Pressable, Button, StyleSheet, TextInput } from 'react-native';
 
-const Socials = () => {
+const Socials = ({ onContinuePress }) => {
+
+    const styles = StyleSheet.create({
+        mainView: {
+            flex: 1,
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+
+        textInput: {
+            height: 50,
+            width: 250,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+        },
+
+    });
+
     return (
-
-        <View>
-            <Text>Socials</Text>
+        <View style={styles.mainView}>
+            <View style={{ bottom: '15%' }}>
+                <Text style={{ fontFamily: 'Roboto', fontSize: 48, textAlign: 'center', }}>Add Socials</Text>
+                <Text style={{ fontFamily: 'Roboto Light', fontSize: 16, textAlign: 'center', }}>You will be sharing these at events</Text>
+            </View>
+            <View style={{}}>
+                <TextInput placeholder={"Instagram"} style={styles.textInput} />
+                <TextInput placeholder={"Twitter"} style={styles.textInput} />
+                <TextInput placeholder={"LinkedIn"} style={styles.textInput} />
+            </View>
+            <Pressable onPress={onContinuePress} style={{ top: '20%' }}>
+                <Image style={{
+                    width: 300,
+                    height: 137,
+                    resizeMode: 'contain',
+                }} source={require('../assets/continue.png')} />
+            </Pressable>
         </View>
     );
 }
